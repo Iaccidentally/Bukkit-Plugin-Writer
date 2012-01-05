a7520b714a0886cd58d09c2044cf5ac69d87ef47
Chris Ward <chris@chrisgward.com>
2012-01-05 12:44:54 +1100

    Reasonably blank maven project.

---
+++ README.md
@@ -0,0 +1,6 @@
+Bukkit plugin writer!
+
+Feel free to help out! Its open source!
+
+
+Code released under the Creative Commons BY-NC licence, available from http://creativecommons.org/licenses/by-nc/3.0/
---
+++ src/main/java/com/chrisgward/bukkit/bukkitpluginwriter/Launcher.java
@@ -0,0 +1,14 @@
+package com.chrisgward.bukkit.bukkitpluginwriter;
+
+/**
+ * Package: com.chrisgward.bukkit.bukkitpluginwriter
+ * By: chris
+ * Date: 5/01/12
+ * Time: 12:41 PM 5/01/12
+ */
+public class Launcher {
+    public static void main(String[] args)
+    {
+
+    }
+}
