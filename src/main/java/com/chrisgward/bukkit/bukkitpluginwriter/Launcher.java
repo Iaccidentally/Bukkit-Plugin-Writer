18a8f0a9786b95212e2d297cab2a8e6ed611e076
Chris Ward <chris@chrisgward.com>
2012-01-05 13:04:29 +1100

    Making splash screen

--- src/main/java/com/chrisgward/bukkit/bukkitpluginwriter/Launcher.java
+++ src/main/java/com/chrisgward/bukkit/bukkitpluginwriter/Launcher.java
@@ -7,8 +7,11 @@ package com.chrisgward.bukkit.bukkitpluginwriter;
  * Time: 12:41 PM 5/01/12
  */
 public class Launcher {
+    public static String[] runtimeArguments;
     public static void main(String[] args)
     {
-
+        runtimeArguments = args;
+        System.out.println("Running the launcher frame");
+        new Splash();
     }
 }
---
+++ src/main/java/com/chrisgward/bukkit/bukkitpluginwriter/Splash.java
@@ -0,0 +1,18 @@
+package com.chrisgward.bukkit.bukkitpluginwriter;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+/**
+ * Package: com.chrisgward.bukkit.bukkitpluginwriter
+ * By: chris
+ * Date: 5/01/12
+ * Time: 1:01 PM 5/01/12
+ */
+public class Splash extends JFrame
+{
+    Splash()
+    {
+
+    }
+}
